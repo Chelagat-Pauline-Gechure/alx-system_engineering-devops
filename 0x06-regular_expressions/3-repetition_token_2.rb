7a948a8162012cb3a15119a428fa6cadbc13fe0b
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-08-01 14:21:50 -0700

    3. Repetition Token #2

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# Accept the argument from the command line
+# Define the regular expression
+# Use the regular expression to find matches
+# Print the matches
+puts ARGV[0].scan(/hbt+n/).join
