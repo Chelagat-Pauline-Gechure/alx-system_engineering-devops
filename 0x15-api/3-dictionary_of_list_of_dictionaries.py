df83db9fb985be3a92f2de120da897b7d428fc9a
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-23 13:26:29 -0700

    3. Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -25,5 +25,5 @@ if __name__ == "__main__":
                 taskList.append(taskDict)
         todoAll[user.get('id')] = taskList
 
-    with open('todo_all_employees.json', mode='w') as f:
-        json.dump(todoAll, f)
+    with open('todo_all_employees.json', mode='w') as filename:
+        json.dump(todoAll, filename)
